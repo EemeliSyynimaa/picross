fa19dd99159965cfdff2aebebe87a6f070b5e2f7
eeneku <eeneku@gmail.com>
2012-11-10 22:48:56 +0200

    Added new functions to engine and made the primary game scene.

--- engine/scene.py
+++ engine/scene.py
@@ -1,11 +1,55 @@
+import pyglet
+import resource_manager
+
 class Scene(object):
     """ Scene class! """
 
-    def __init__(self):
+    def __init__(self, manager):
+        self.manager = manager
+        self.batch = pyglet.graphics.Batch()
+        self.resource_manager = resource_manager.ResourceManager()
+        
+    def on_key_press(self, symbol, modifiers):
+        pass
+    
+    def on_key_release(self, symbol, modifiers):
         pass
         
-    def update(self, dt):
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        pass 
+    
+    def on_mouse_enter(self, x, y):
+        pass  
+    
+    def on_mouse_leave(self, x, y):
+        pass 
+    
+    def on_mouse_motion(self, x, y, dx, dy):
+        pass 
+    
+    def on_mouse_press(self, x, y, button, modifiers):
+        pass 
+    
+    def on_mouse_release(self, x, y, button, modifiers):
+        pass
+     
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        pass 
+    
+    def on_text(self, text):
+        pass
+    
+    def on_text_motion(self, motion):
+        pass
+    
+    def on_text_motion_select(self, motion):
         pass
     
     def on_draw(self):
+        self.batch.draw()
+        
+    def update(self, dt):
+        pass
+        
+    def cleanup(self):
         pass
--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-import engine
+from engine import engine
+from scenes import game
 
 class App(object):
     """ The main class that ties everything together. """
     
     def __init__(self):
         self.engine = engine.Engine(width=1280, height=720)
+        self.engine.scene_manager.add_scene("game", game.Game)
+        
+        self.engine.run()
         
 if __name__ == '__main__':
     app = App()  

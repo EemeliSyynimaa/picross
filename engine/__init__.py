11c00c240b1f69b27988cc6443226a1f9cb8d6dd
eeneku <eeneku@gmail.com>
2012-11-10 13:30:43 +0200

    Updated engine.

---
+++ engine/__init__.py
@@ -0,0 +1,16 @@
+# Imports
+import pyglet
+
+class Engine:
+    """ The main engine class. """
+    def __init__(self):
+        """ Initializes the engine. """
+        pass
+    
+    def update(self):
+        """ Updates all game actions. """
+        pass
+    
+    def draw(self):
+        """ Draws all the stuff. """
+        pass

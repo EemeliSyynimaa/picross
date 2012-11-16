944a9950d5d42fe074bc7c963feb3f277469c92f
eeneku <eeneku@gmail.com>
2012-11-16 19:27:06 +0200

    Modified Grid a bit.

--- grid.py
+++ grid.py
@@ -1,12 +1,19 @@
 class Grid(object):
     """ This class holds the grid of the game. """
 
-    def __init__(self, path):
+    def __init__(self, path, resource_manager):
         self.x = 0
         self.y = 0
         self.path = path
+        self.resource_manager = resource_manager
+        
+        self.init_resources()
+        
+    def init_resources(self):
+        self.resource_manager.load_image("tile_down.png")
+        self.resource_manager.load_image("tile_up.png")
     
-    def on_draw(self):
-        pass
+    def draw(self):
+        self.resource_manager.gfx["tile_down"].blit(0, 0)
     
         
--- scenes/game.py
+++ scenes/game.py
@@ -8,12 +8,12 @@ class Game(scene.Scene):
     def __init__(self, manager, path):
         super(Game, self).__init__(manager)
         
-        self.grid = grid.Grid(path)
+        self.grid = grid.Grid(path, self.resource_manager)
         self.init_resources()
         
     def init_resources(self):
-        self.resource_manager.load_image("tile_down.png")
-        self.resource_manager.load_image("tile_up.png")
+        pass
    
     def on_draw(self):
         self.batch.draw()
+        self.grid.draw()
